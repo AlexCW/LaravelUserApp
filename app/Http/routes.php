9591597538d694c8240863958e14144a16e4a964
Alex Williams <alex.williams@kaweb.co.uk>
2015-07-20 23:14:55 +0100

    Add a logout route and make the home route load the welcome page for now

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,7 +15,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/home', function () {
+    return view('welcome');
+});
+
+//Rather than controller that creates messy routes in this case.
 Route::get('auth/register', 'Auth\AuthController@getRegister');
 Route::post('auth/register', 'Auth\AuthController@postRegister');
+
 Route::get('auth/login', 'Auth\AuthController@getLogin');
 Route::post('auth/login', 'Auth\AuthController@postLogin');
+
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
