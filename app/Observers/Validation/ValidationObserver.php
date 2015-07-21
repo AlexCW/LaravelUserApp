06e8f6e717e4602d084baaf9d9441aa6cbad9ae3
Alex Williams <alex.williams@kaweb.co.uk>
2015-07-21 23:17:50 +0100

    Had set wrong function name

--- app/Observers/Validation/ValidationObserver.php
+++ app/Observers/Validation/ValidationObserver.php
@@ -56,5 +56,5 @@ class ValidationObserver {
 
     protected function setConditionalRules(\Eloquent $model){}
 
-    protected function setAdditionalData(){}
+    protected function getAdditionalData(){}
 }
